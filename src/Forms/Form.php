c54f61050883a6d8ef73e09dadad8cbf6c5969ac
Eric Lamb <eric@mithra62.com>
2022-03-14 13:21:03 -0700

    adds basic properties

---
+++ src/Forms/Form.php
@@ -0,0 +1,35 @@
+<?php
+namespace EeObjects\Forms;
+
+class Form
+{
+    protected $save_btn_text = 'save';
+
+    protected $save_btn_text_working = 'saving';
+
+    protected $ajax_validate = false;
+
+    protected $has_file_input = false;
+
+    protected $alerts_name = '';
+
+    protected $form_hidden = [];
+
+    protected $cp_page_title_alt = false;
+
+    protected $cp_page_title = '';
+
+    protected $action_button = [];
+
+    protected $hide_top_buttons = [];
+
+    protected $extra_alerts = [];
+
+    protected $buttons = [];
+
+    protected $base_url = '';
+
+    protected $sections = [];
+
+    protected $tabs = [];
+}
